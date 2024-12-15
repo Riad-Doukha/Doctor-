64af85c921bb8cf8b3ddeebf9ad876b2b1b0aefa
Riad-doukha <doukhamohamedriad@gmail.com>
2024-12-15 17:21:37 +0100

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      extend: {
+        boxShadow: {
+          'custom': '0px 24px 32px -3px rgba(3, 9, 50, 0.04)',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
